9a8a438cabe375f9abf440e0c90f931e365ad07c
Chris Simpkins <chris.simpkins@gmail.com>
2017-03-07 22:21:55 -0500

    Update pandas slides, add hotjobs.py

---
+++ code/analytics/hotjobs.py
@@ -0,0 +1,17 @@
+salary = {"Data Scientist": 110000,
+          "DevOps Engineer": 110000,
+          "Data Engineer": 106000,
+          "Analytics Manager": 112000,
+          "Database Administrator": 93000,
+          "Software Architect": 125000,
+          "Software Engineer": 101000,
+          "Supply Chain Manager": 100000}
+
+openings = {"Data Scientist": 4184,
+            "DevOps Engineer": 2725,
+            "Data Engineer": 2599,
+            "Analytics Manager": 1958,
+            "Database Administrator": 2877,
+            "Software Architect": 2232,
+            "Software Engineer": 17085,
+            "Supply Chain Manager": 1270}
